95f4dfac1c581174f8d5a6d4a2ff2a22774dee9d
sean <sezhang@aarp.org>
2018-10-18 09:25:49 -0400

    added adapters

---
+++ app/src/main/java/com/seanzhang/smellslikebakin/ListFragment.java
@@ -0,0 +1,34 @@
+package com.seanzhang.smellslikebakin;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ListFragment extends Fragment {
+
+    public interface OnRecipeSelectedInterface {
+        void onListRecipeSelected(int index);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        OnRecipeSelectedInterface listener = (OnRecipeSelectedInterface) getActivity();
+
+        View view = inflater.inflate(R.layout.fragment_list, container, false);
+        RecyclerView recyclerView = view.findViewById(R.id.listRecyclerView);
+        ListAdapter listAdapter = new ListAdapter(listener);
+        recyclerView.setAdapter(listAdapter);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(layoutManager);
+        return  view;
+
+    }
+}
